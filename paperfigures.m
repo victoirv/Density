b626351f8c982e099d1da7ba7e2edc603aa7b79d
victoirv <victoirv@gmail.com>
2016-07-01 16:48:45 -0400

    Cleaning things up for paper

--- paperfigures.m
+++ paperfigures.m
@@ -1,8 +1,7 @@
 function paperfigures
 
 Density(1,6) %alldata, ccplot, stormavs-dst, F107MD27d
-Density(24,6) %RhoBinned case24
+Density(24,6) %RhoBinned case24, mass-gt20
 Density(10,6) %dst-50-tak, and significance table
 Density(5,6) %dd12
 Density(13,6) %dst-day
-Density(24,6) %mass-gt20
